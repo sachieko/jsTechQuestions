f269c5a778f75c21bb83eef371874cf9956992ef
Chelsea Sachieko <arashikabane@gmail.com>
2023-09-22 11:49:57 -0600

    Add dev branch to check solutions

--- evaluate.js
+++ evaluate.js
@@ -1,6 +1,6 @@
 // Do not modify this file, run using `node evaluate.js` to test your functions
 import  chalk  from  'chalk';
-import { isPalindrome, maxSubMatrix } from "./technicalQuestions.js";
+import { isPalindrome, maxSubMatrix } from "./solutions.js";
 
 const passString = chalk.green('Case Passed\n');
 const failString = chalk.red('Case Failed\n');
